0b8fea07242b15c29457d3f2f1e35b05dbe1967f
Tsachi Shlidor <tsi.shlidor@gmail.com>
2019-01-10 17:21:48 +0200

    demo

---
+++ docs/precache-manifest.b36e6704a184f3aa9c484b56fea8cbe2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebc39cc0003328b4f181a4835f5f8e84",
+    "url": "/static/media/cloudinary_logo_for_white_bg.ebc39cc0.svg"
+  },
+  {
+    "revision": "a0264c6ba24524c831294d05e6cf8324",
+    "url": "/static/media/cloudinary_logo_for_dark_bg.a0264c6b.svg"
+  },
+  {
+    "revision": "3891bec1aecd5a2bbddc1b67ce8412aa",
+    "url": "/static/media/cloudinary_icon_for_white_bg.3891bec1.svg"
+  },
+  {
+    "revision": "e0662a9ecd582800b8d8b1304ca59191",
+    "url": "/static/media/cloudinary_icon_for_black_bg.e0662a9e.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc936b714491ed7bec40",
+    "url": "/static/js/main.fc936b71.chunk.js"
+  },
+  {
+    "revision": "5771dba659117c5bc365",
+    "url": "/static/js/1.5771dba6.chunk.js"
+  },
+  {
+    "revision": "fc936b714491ed7bec40",
+    "url": "/static/css/main.5d75c229.chunk.css"
+  },
+  {
+    "revision": "5771dba659117c5bc365",
+    "url": "/static/css/1.aad22553.chunk.css"
+  },
+  {
+    "revision": "f70259b712954d404c0e337bfb24b21a",
+    "url": "/index.html"
+  }
+];
